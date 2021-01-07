21178b4f0363270c5879cbc302dbe726397031f1
William Magland <william.magland@gmail.com>
2021-01-07 10:53:50 -0500

    removed thewall, reflection functions. added string support for botcommands to save space

--- JoyClient.py
+++ JoyClient.py
@@ -13,22 +13,15 @@ qst = rnd.choice(questions)
 qstq = qst.question
 qstex = qst.example
 
-reflection_embed = discord.Embed(title='Daily Reflection', description='Take this time to reflect on your day. What did you accomplish? Answer todays question below.', color=0xff6bc9)
-reflection_embed.add_field(name="Today's Question:", value=qstq, inline=True)
-reflection_embed.add_field(name='example:', value=qstex, inline=False)
-reflection_embed.set_footer(text='add ">" to the start of your response to add it to the wall')
-
 help_embed = discord.Embed(title='Joyous Help', description="Hi! I'm Joyous, your discord positivity bot!", color=0xff6bc9)
 help_embed.add_field(name='What do I do?', value="I keep your server positive with a daily reflection. I also have a wall where people can save their responses.")
 help_embed.add_field(name="How do I use Joyous?", value="You can talk to me by using '>' and whatever you want me to do. You can find a list of commands with >commands.")
 help_embed.set_footer(text='under progress by willy! you can find the project here https://github.com/CuteBlueRadio/Joyous')
 
 wall_embed = discord.Embed(title='The Wall', description="Here's where all of your opted daily reflection responses are stored! Everyone can see this.", color=0xff6bc9)
-# wall_embed.add_field(name=)
 
 comand_prefix = '>'
 
-current_question = qstq
 
 async def hello(words, trigger, message):
     await message.channel.send('Hello!!')
@@ -46,34 +39,10 @@ async def random(words, trigger, message):
 async def all(words, trigger, message):
     joys = fetch_joys()
     await message.channel.send(f'here are all the joys in my library......{joys}')
-    
-async def daily_joy(words, trigger, message):
-    joys = fetch_joys()
-    await message.channel.send(f"""Goooooood Morning!! Here's todays joy:
-I'm grateful for...**{rnd.choice(joys)}**""")
-
-async def reflection(words, trigger, message):
-    await message.channel.send(embed = reflection_embed)
 
 async def help(words, trigger, message):
     await message.channel.send(embed = help_embed)
     awaiting_response = True
-
-async def success(x, message):
-    success_embed = discord.Embed()
-    success_embed.add_field(name="Success", value=f'Response "{x}" successfully saved!')
-    success_embed.set_footer(text='try ">thewall" to see them all!')
-    await message.channel.send(embed = success_embed)
-
-async def thewall(words, trigger, message):
-    with open('responses.json', 'r') as f:
-        x = json.load(f)
-        print(current_question)
-        await message.channel.send(x[0][current_question])
-        # await message.channel.send(embed=wall_embed)
-
-async def panic(words, trigger, message):
-    await message.channel.send('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!')
 
 async def gaytest(words, trigger, message):
     x = ' '.join(words[1:])
@@ -95,8 +64,11 @@ class botcommand:
         self.response = response
     async def handle(self, message, words):
         if words[0] == self.trigger:
-            await self.response(words, self.trigger, message)
-            return True
+            if type(self.response) == str:
+                await message.channel.send(self.response)
+            else:
+                await self.response(words, self.trigger, message)
+                return True
         else:
             return False
 
@@ -105,11 +77,9 @@ command_list = [
     botcommand('add', add),
     botcommand('random', random),
     botcommand('all', all),
-    botcommand('test', reflection),
     botcommand('help', help),
-    botcommand('thewall', thewall),
     botcommand('amigay', gaytest),
-    botcommand('panic', panic),
+    botcommand('sing', 'lalala')
 ]
 
 class reaction:
@@ -132,7 +102,6 @@ class Client(discord.Client):
         print(self.user.name)
         print(self.user.id)
         print('--------------')
-        # print('THIS THING:',current_question)
 
     async def on_message(self, message: discord.Message):
         content = message.content
@@ -152,27 +121,6 @@ class Client(discord.Client):
             for command in command_list:
                 if (await command.handle(message, words)) is True:
                     return
-
-            if message.author not in author_blacklist:
-                if current_question is not None:
-                    with open('responses.json', 'r') as f:
-                        x = json.load(f)
-                    for s in x:
-                        print('woop woop')
-                        if s[current_question] == True:
-                            print('reeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-                            x[current_question].append({
-                                str(current_question): [content[1:]]
-                            })
-                    else:
-                        print('nooooooooooooooooooooooooooooo')
-                        x.append({
-                            str(current_question): [content[1:]]
-                        })
-                    with open('responses.json', 'w') as f:
-                        json.dump(x, f)
-                    author_blacklist.append(message.author)
-                    await success(content[1:], message)
 
 client = Client()
 
